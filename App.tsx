5d13b724fa6dc66d59225d8ab7f9f6ff465b289e
Debora <debora.ribeirodasilva25@gmail.com>
2021-08-26 14:51:29 -0400

    Imagens incluidas no meu header

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World !!!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+          <Image source={
+            require("./src/assets/arrow-left.png")
+            }>
+          </Image>
+          <Image source={
+            require("./src/assets/ei_search.png")
+            }>
+          </Image>
+          <Image source={
+            require("./src/assets/share.png")
+            }>
+          </Image>
+      </View>
     </View>
   );
 }
@@ -14,8 +27,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFF',
   },
+
+  header: {
+    minHeight: 70,
+    backgroundColor: '#436980',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 30,
+    paddingVertical:30,
+  },
+
 });
